e2590122a84a38440883ff6caf48d0d4b7ec4168
zukz <zukz@bitbucket.org>
2024-02-06 14:54:08 -0500

    agregando pruebas unitarias de listar planetas y agregando capa controladora

--- __test__/planetas/findAllPlanets.unit.test.js
+++ __test__/planetas/findAllPlanets.unit.test.js
@@ -1,14 +1,22 @@
 const { default: expect } = require("expect");
 const { findAllPlanets } = require("../../src/functions/planetas/index");
 
-describe("findAllPlanets", () => {
-  describe("get route", () => {
-    describe("planet not exist", () => {
-      it("should return a 404", () => {
-        expect(true).toBe(true);
-      });
-    });
-  });
+const { isValidJson } = require("../../src/validations/index");
+
+const {
+  listarPlanetasSwapi,
+  encontrarPlaneta,
+  guardarPlaneta,
+} = require("../../src/controllers/index");
+
+test("listarPlanetas return json", async () => {
+  const expected = true;
+  const awsRequestId = "aws-123";
+  const url = "/planets";
+
+  const response = await listarPlanetasSwapi(awsRequestId, url);
+  console.log(response);
+  //expect().toEqual(expected);
 });
 
 test("Reponse is a objecto", () => {
